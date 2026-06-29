e6aa0f47fc3562fa21fbf94bbd5330abfa4e9a87
PranoySarker <pranoysarker01@gmail.com>
2026-06-29 22:59:23 +0600

    some issues fixes. dotenv first then the prisma with tsconfig file

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,9 @@
 import express from 'express';
-import 'dotenv/config';
+
 
 const app = express();
 
-app.use(express.json())
+app.use(express.json());
 
 app.get('/health', (req, res) => {
     res.status(200).json({
